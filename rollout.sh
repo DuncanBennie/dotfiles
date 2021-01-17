f1854c4b35aaeb3150ee51ac07fcb2f59437f91f
Duncan Bennie <duncan@duncanbennie.com>
2021-01-17 19:40:25 +1000

    Made .zshrc section a function

--- rollout.sh
+++ rollout.sh
@@ -8,25 +8,27 @@ NC='\033[0m'
 # Variables
 ZSH=~/.zshrc
 
-echo ""
+zshrc(){
+echo "Copying .zshrc to '$ZSH'."
+if [[ -f "$ZSH" ]]; then
+    read -p "$(echo -e "${RED}.zshrc already exists at '$ZSH'.${NC} Do you want to overwrite it (y/n)? ")" choice
+    if [[ "$choice" = "y" ]]; then
+        ln -sf ./zshrc ~/.zshrc
+        echo -e "${GREEN}.zshrc has been symlinked to '$ZSH'.${NC}"
+    else
+        echo "Skipping .zshrc."
+    fi
+else
+    ln -sf ./zshrc ~/.zshrc
+    echo "${GREEN}.zshrc has been symlinked to '$ZSH'.${NC}"
+fi
+}
+
 read -p "This script will overwrite your pre-existing files. Do you want to continue (y/n)? " choice
 if [[ "$choice" = "y" ]]; then
     echo "Rolling out dotfiles..."
     echo -e "\n"
-    echo "Copying .zshrc to '$ZSH'."
-    if [[ -f "$ZSH" ]]; then
-        read -p "$(echo -e "${RED}.zshrc already exists at '$ZSH'.${NC} Do you want to overwrite it (y/n)? ")" choice
-        if [[ "$choice" = "y" ]]; then
-            ln -sf ./zshrc ~/.zshrc
-            echo -e "${GREEN}.zshrc has been symlinked to '$ZSH'.${NC}"
-        else
-            echo "Skipping .zshrc."
-        fi
-    else
-        ln -sf ./zshrc ~/.zshrc
-        echo "${GREEN}.zshrc has been symlinked to '$ZSH'.${NC}"
-    fi
-    echo -e "\n"
+    zshrc
     echo "Rollout complete."
 else
     echo "Exiting...";
