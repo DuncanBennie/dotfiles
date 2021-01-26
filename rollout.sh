407e15cacf3d5b19a5924b9fdf00bfa37056b303
Duncan Bennie <duncan@duncanbennie.com>
2021-01-26 14:18:23 +1000

    Removed coloured text

--- rollout.sh
+++ rollout.sh
@@ -1,33 +1,26 @@
 #! /bin/bash
 
-# Constants
-RED='\033[0:31m'
-GREEN='\033[0:32m'
-NC='\033[0m'
-
-# Variables
 ZSH=~/.zshrc
 
-zshrc(){
+zshrc () {
 echo "Copying .zshrc to '$ZSH'."
 if [[ -f "$ZSH" ]]; then
-    read -p "$(echo -e "${RED}.zshrc already exists at '$ZSH'.${NC} Do you want to overwrite it (y/n)? ")" choice
+    read -p "$(echo -e ".zshrc already exists at '$ZSH'. Do you want to overwrite it (y/n)? ")" choice
     if [[ "$choice" = "y" ]]; then
-        ln -sf ./zshrc ~/.zshrc
-        echo -e "${GREEN}.zshrc has been symlinked to '$ZSH'.${NC}"
+        ln -sf ./.zshrc ~/.zshrc
+        echo -e ".zshrc has been symlinked to '$ZSH'."
     else
         echo "Skipping .zshrc."
     fi
 else
-    ln -sf ./zshrc ~/.zshrc
-    echo "${GREEN}.zshrc has been symlinked to '$ZSH'.${NC}"
+    ln -sf ./.zshrc ~/.zshrc
+    echo ".zshrc has been symlinked to '$ZSH'."
 fi
 }
 
 read -p "This script will overwrite your pre-existing files. Do you want to continue (y/n)? " choice
 if [[ "$choice" = "y" ]]; then
     echo "Rolling out dotfiles..."
-    echo -e "\n"
     zshrc
     echo "Rollout complete."
 else
